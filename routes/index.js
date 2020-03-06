a36e629e8a3ce49ca0e4e2d6bb87a2e06801a476
FTM <dschleicherwbs@gmail.com>
2020-03-06 15:51:14 +0100

    first commit

---
+++ routes/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+var multer = require('multer');
+var storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, process.env.IMAGE_PATH);
+  },
+  filename: function(req, file, cb) {
+    cb(null, Date.now() + '_' + file.originalname);
+  }
+});
+var upload = multer({ storage });
+
+router.post('/upload', upload.single('file'), (req, res, next) => {
+  try {
+    console.log(req.file);
+
+    if (!req.file) {
+      res.status(500).json(req);
+    }
+    res
+      .status(200)
+      .json({ fileUrl: process.env.BASE_PATH + req.file.originalname });
+  } catch (error) {
+    return next(error);
+  }
+});
+
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+module.exports = router;
