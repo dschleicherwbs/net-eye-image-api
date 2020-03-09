1562a50a28f8c50c1d2151669851b950406657ff
FTM <dschleicherwbs@gmail.com>
2020-03-09 15:51:36 +0100

    minor changes

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var storage = multer.diskStorage({
     cb(null, process.env.IMAGE_PATH);
   },
   filename: function(req, file, cb) {
-    cb(null, Date.now() + '_' + file.originalname);
+    cb(null, file.originalname);
   }
 });
 var upload = multer({ storage });
@@ -21,15 +21,15 @@ router.post('/upload', upload.single('file'), (req, res, next) => {
     }
     res
       .status(200)
-      .json({ fileUrl: process.env.BASE_PATH + req.file.originalname });
+      .json({ fileUrl: process.env.BASE_PATH + req.file.filename });
   } catch (error) {
     return next(error);
   }
 });
 
-// /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 
 module.exports = router;
